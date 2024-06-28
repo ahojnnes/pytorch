5de918f28608a52a7bd06f5b19d80aef93130a6d
Pearu Peterson <pearu.peterson@gmail.com>
2024-06-28 09:27:36 +0300

    Add pinned memory support to sparse COO/CSR/CSC/BSR/BSC tensors
    ghstack-source-id: b3241252237ffd9a2c6cf009d36dfc83a41a3cb0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129645

--- aten/src/ATen/cuda/PinnedMemoryAllocator.cpp
+++ aten/src/ATen/cuda/PinnedMemoryAllocator.cpp
@@ -1,6 +1,7 @@
 #include <ATen/cuda/PinnedMemoryAllocator.h>
 #include <ATen/Context.h>
 #include <ATen/Config.h>
+#include <ATen/SparseCsrTensorUtils.h>
 #include <ATen/TensorUtils.h>
 #include <c10/core/Storage.h>
 #include <ATen/ATen.h>
@@ -12,6 +13,16 @@ bool is_pinned_cuda(const Tensor& self, std::optional<Device> device) {
   TORCH_INTERNAL_ASSERT_DEBUG_ONLY(!device.has_value() || device->is_cuda());
   // TODO: unhook this
   return detail::getCUDAHooks().isPinnedPtr(self.storage().data());
+}
+
+bool is_pinned_sparse_coo(const Tensor& self, std::optional<Device> device) {
+  // Assuming that _indices has the same pin memory state as _values
+  return self._values().is_pinned(device);
+}
+
+bool is_pinned_sparse_compressed(const Tensor& self, std::optional<Device> device) {
+  // Assuming that compressed/plain_indices has the same pin memory state as values
+  return self.values().is_pinned(device);
 }
 
 Tensor _pin_memory_cuda(const Tensor& self, std::optional<Device> device) {
@@ -28,5 +39,33 @@ Tensor _pin_memory_cuda(const Tensor& self, std::optional<Device> device) {
   return tensor;
 }
 
+Tensor _pin_memory_sparse_compressed(const Tensor& self, std::optional<Device> device) {
+  TORCH_INTERNAL_ASSERT_DEBUG_ONLY(!device.has_value() || device->is_cuda());
+  at::sparse_csr::CheckSparseTensorInvariants _(false);
+  TensorOptions options = self.options().pinned_memory(true);
+  auto impl = at::sparse_csr::get_sparse_csr_impl(self);
+  return at::_sparse_compressed_tensor_unsafe(
+        impl->compressed_indices().pin_memory(device),
+        impl->plain_indices().pin_memory(device),
+        impl->values().pin_memory(device),
+        self.sizes(),
+        optTypeMetaToScalarType(options.dtype_opt()),
+        options.layout_opt(),
+        options.device_opt(),
+        options.pinned_memory_opt());
+}
+
+Tensor _pin_memory_sparse_coo(const Tensor& self, std::optional<Device> device) {
+  TORCH_INTERNAL_ASSERT_DEBUG_ONLY(!device.has_value() || device->is_cuda());
+  TensorOptions options = self.options().pinned_memory(true);
+  return at::_sparse_coo_tensor_with_dims_and_tensors(
+      self.sparse_dim(),
+      self.dense_dim(),
+      self.sizes(),
+      self._indices().pin_memory(device),
+      self._values().pin_memory(device),
+      options,
+      self.is_coalesced());
+}
 
 } // namespace at::native
